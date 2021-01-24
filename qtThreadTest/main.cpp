cb11c00a6f09a6564e84cb6c90870165d7a6313e
lvpeng <1217869072@qq.com>
2021-01-24 23:39:23 +0800

    多线程测试

--- qtThreadTest/main.cpp
+++ qtThreadTest/main.cpp
@@ -22,22 +22,60 @@ class TestTask : public QRunnable
 	}
 };
 
-
+int mapped(int i) {
+	QThread::currentThread()->sleep(i);
+	return i + 10;
+}
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-
 	TestTask* task = new TestTask;
 	QThreadPool::globalInstance()->start(task);
-	//concurrent::TestThread thread;
-	//thread.start();
-	//thread.wait();
-	QtConcurrent::run([]() {
-		qDebug() << "concurrent线程" << QThread::currentThreadId();
+
+	QLabel * label = new QLabel();
+
+	label->setText("0");
+	label->resize(200, 200);
+	label->show();
+
+	QFutureWatcher<int> watcher;
+
+	QObject::connect(&watcher, &QFutureWatcher<int>::progressValueChanged, label, [label](int progress) {
+		label->setText(QString::number(progress));
 	});
+	QObject::connect(&watcher, &QFutureWatcher<int>::finished, label, [label]() {
+		label->setText("结束");
+	});
+
+	QList<int> intList;
+	for (size_t i = 0; i < 10; i++)
+	{
+		intList.push_back(i);
+	}
+	QFuture<int> future = QtConcurrent::mapped(intList, mapped);
+
+
+	//QFuture<int> future = QtConcurrent::run([label]() -> int {
+	//	qDebug() << "concurrent线程" << QThread::currentThreadId();
+	//	for (int i = 1; i < 10; i++)
+	//	{
+	//		//label->setText(QString::number(i));
+	//		qDebug() << i;
+	//		QThread::currentThread()->sleep(1);
+	//	}
+	//	return 20;
+	//});
+
+	watcher.setFuture(future);
+	qDebug() << "result" << watcher.future().results();
+
+	//int res = future.result();
+
+	//qDebug() << "res" << res;
+
+
 	qDebug() << "这是在GUI主线程里：" << app.thread()->currentThreadId();
-	//qDebug() << concurrent::ThreadPool::instance()->a;
-	concurrent::run([]() {
+	concurrent::run([label]() {
 		qDebug() << "run in 2322" << QThread::currentThreadId();
 	});
 	return app.exec();
